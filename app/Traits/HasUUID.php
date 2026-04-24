bf3aac5ece1468fa89c65772ad88184a087cb212
taha-abbasali <tahaabbasali2@gmail.com>
2026-04-24 14:47:54 +0500

    breeze authentication and RBAC related table migrations done

---
+++ app/Traits/HasUUID.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasUUID
+{
+    protected static function bootHasUUID()
+    {
+        static::creating(function ($model) {
+           if (empty($model->{$model->primaryKey})) {
+    $model->{$model->primaryKey} = Str::uuid();
+}
+        });
+    }
+
+
+    public function initializeHasUUID()
+    {
+        $this->keyType = 'string';
+        $this->incrementing = false;
+        $this->casts = [
+            ...$this->casts,
+            $this->primaryKey => 'string',
+        ];
+    }
+}
